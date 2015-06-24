62f676e53cc0317e23d3ed47b1075a9949620b1e
minhduc170385 <user1@TGs-MacBook-Pro.local>
2015-06-24 10:47:02 -0700

    aaa

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TragnGau2
+//
+//  Created by TG on 6/24/15.
+//  Copyright (c) 2015 TG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
